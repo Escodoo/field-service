1ddd1a2c9f79a5519ce689e92b490d108fa3eafa
Marcel Savegnago <marcel.savegnago@gmail.com>
2023-06-12 19:03:35 -0300

    [MIG] fieldservice_rating: Migration to 14.0

--- fieldservice_rating/__manifest__.py
+++ fieldservice_rating/__manifest__.py
@@ -5,7 +5,7 @@
     "name": "Fieldservice Rating",
     "summary": """
         This module enable field service order rating""",
-    "version": "12.0.1.0.0",
+    "version": "14.0.1.0.0",
     "license": "AGPL-3",
     "author": "Escodoo,Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/field-service",
@@ -17,6 +17,7 @@
         "data/fieldservice_data.xml",
         "views/fsm_order.xml",
         "views/fsm_stage.xml",
+        "views/fsm_order_menu.xml",
     ],
     "demo": [
         "demo/fsm_order.xml",

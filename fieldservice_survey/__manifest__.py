3d8dac4131675ec3778a631a1d6a5b5d9cc6a595
Marcel Savegnago <marcel.savegnago@gmail.com>
2023-06-27 09:53:08 -0300

    teste

--- fieldservice_survey/__manifest__.py
+++ fieldservice_survey/__manifest__.py
@@ -5,7 +5,7 @@
     "name": "Fieldservice Survey",
     "summary": """
         This module enable integration the fieldservice app with survey""",
-    "version": "14.0.1.0.0",
+    "version": "12.0.1.0.0",
     "license": "AGPL-3",
     "author": "Escodoo,Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/field-service",
@@ -21,12 +21,12 @@
         "views/survey_user_input.xml",
         "views/fsm_order.xml",
         "views/survey_templates_statistics.xml",
-        "data/fsm_order_survey_data.xml",
+        # "data/fsm_order_survey_data.xml",
         "data/mail_data.xml",
         "wizard/fsm_order_survey_submit.xml",
     ],
-    "demo": [
-        "demo/fsm_stage.xml",
-        "demo/fsm_order.xml",
-    ],
+    # "demo": [
+        # "demo/fsm_stage.xml",
+        # "demo/fsm_order.xml",
+    # ],
 }
